6faa885073851f560de5f744b66c7051b880c46f
Rafael <rafaelgermanio@gmail.com>
2024-05-15 20:26:39 -0300

    feat: Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return None
+
+    try:
+        with open(path_file, "r") as file:
+            content = file.read()
+            return content.split("\n")
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
+
+
+print(txt_importer("statics/arquivo_teste.txt"))
